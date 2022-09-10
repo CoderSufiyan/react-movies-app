416ef3e261459ad86eac09b3e93d25319056e954
codersufiyan <sufiyanandkhan@gmail.com>
2022-09-10 21:55:25 +0530

    added List component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
+import List from './Components/List';
 import React from 'react';
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
     <React.Fragment>
       <Navbar/>
       <Banner/>
+      <List/>
     </React.Fragment>
   );
 }
